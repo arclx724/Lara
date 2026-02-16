064ddc810697524fb6c1af9c3f591a0445cfac21
arclx724 <arclx724@gmail.com>
2026-02-16 19:14:46 +0530

    Update mongochk.py

--- ShrutiMusic/plugins/tools/mongochk.py
+++ ShrutiMusic/plugins/tools/mongochk.py
@@ -1,37 +1,13 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
 import re
-
 from pymongo import MongoClient
 from pyrogram import filters
 from pyrogram.types import Message
-
 from ShrutiMusic import app
+from ShrutiMusic.misc import SUDOERS # SECURITY PATCH: Import SUDOERS
 
 mongo_url_pattern = re.compile(r"mongodb(?:\+srv)?:\/\/[^\s]+")
 
-
-@app.on_message(filters.command("mongochk"))
+@app.on_message(filters.command("mongochk") & SUDOERS) # SECURITY PATCH: Restricted to Sudoers
 async def mongo_command(client, message: Message):
     if len(message.command) < 2:
         await message.reply(
@@ -41,16 +17,16 @@ async def mongo_command(client, message: Message):
 
     mongo_url = message.command[1]
     if re.match(mongo_url_pattern, mongo_url):
+        msg = await message.reply("⏳ ᴄʜᴇᴄᴋɪɴɢ ᴍᴏɴɢᴏᴅʙ ᴄᴏɴɴᴇᴄᴛɪᴏɴ...")
         try:
             # Attempt to connect to the MongoDB instance
-            client = MongoClient(mongo_url, serverSelectionTimeoutMS=5000)
-            client.server_info()  # Will cause an exception if connection fails
-            await message.reply("ᴍᴏɴɢᴏᴅʙ ᴜʀʟ ɪs ᴠᴀʟɪᴅ ᴀɴᴅ ᴄᴏɴɴᴇᴄᴛɪᴏɴ sᴜᴄᴇssғᴜʟ ✅")
+            m_client = MongoClient(mongo_url, serverSelectionTimeoutMS=5000)
+            m_client.server_info()  # Will cause an exception if connection fails
+            await msg.edit_text("ᴍᴏɴɢᴏᴅʙ ᴜʀʟ ɪs ᴠᴀʟɪᴅ ᴀɴᴅ ᴄᴏɴɴᴇᴄᴛɪᴏɴ sᴜᴄᴇssғᴜʟ ✅")
         except Exception as e:
-            await message.reply(f"ғᴀɪʟᴇᴅ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ᴍᴏɴɢᴏᴅʙ: {e}")
+            await msg.edit_text(f"❌ ғᴀɪʟᴇᴅ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ᴍᴏɴɢᴏᴅʙ: {e}")
     else:
-        await message.reply("ᴜᴘs! ʏᴏᴜʀ ᴍᴏɴɢᴏᴅʙ ғᴏʀᴍᴀᴛ ɪs ɪɴᴠᴀʟɪᴅ")
-
+        await message.reply("❌ ᴜᴘs! ʏᴏᴜʀ ᴍᴏɴɢᴏᴅʙ ғᴏʀᴍᴀᴛ ɪs ɪɴᴠᴀʟɪᴅ")
 
 __MODULE__ = "Mᴏɴɢᴏᴅʙ"
 __HELP__ = """
@@ -58,15 +34,3 @@ __HELP__ = """
 
 • `/mongochk [mongo_url]`: Cʜᴇᴄᴋs ᴛʜᴇ ᴠᴀʟɪᴅɪᴛʏ ᴏғ ᴀ ᴍᴏɴɢᴏᴅʙ URL ᴀɴᴅ ᴄᴏɴɴᴇᴄᴛɪᴏɴ ᴛᴏ ᴛʜᴇ ᴍᴏɴɢᴏᴅʙ ɪɴsᴛᴀɴᴄᴇ.
 """
-
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
